c0a510b2d651bd98fd54f0874142dbf931236eea
david345672 <43988987+david345672@users.noreply.github.com>
2022-02-10 11:19:17 +0100

    update forms

--- Proyecto2Desktop/DesktopApp/DesktopApp/FormListaSkills.Designer.cs
+++ Proyecto2Desktop/DesktopApp/DesktopApp/FormListaSkills.Designer.cs
@@ -211,6 +211,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1445, 731);
             this.Controls.Add(this.btnGestionarGrupos);
             this.Controls.Add(this.cbxBcolors);
