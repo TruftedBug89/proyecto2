49910a00b51c5b73195b9d3da26d8e5e6e9da2ed
TruftedBug89 <39797722+TruftedBug89@users.noreply.github.com>
2022-02-11 11:57:55 +0100

    Form KPI i Crear Usuarios

--- Proyecto2Desktop/DesktopApp/DesktopApp/FormCrearUsuarios.cs
+++ Proyecto2Desktop/DesktopApp/DesktopApp/FormCrearUsuarios.cs
@@ -16,5 +16,15 @@ namespace DesktopApp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormCrearUsuarios_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
