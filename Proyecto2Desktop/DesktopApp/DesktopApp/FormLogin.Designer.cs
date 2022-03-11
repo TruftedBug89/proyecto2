a190a8dd6ee0dee191f0026b4844cfb4dcc885f8
marcph100 <98086516+marcph100@users.noreply.github.com>
2022-03-11 12:30:36 +0100

    LoginDone

--- Proyecto2Desktop/DesktopApp/DesktopApp/FormLogin.Designer.cs
+++ Proyecto2Desktop/DesktopApp/DesktopApp/FormLogin.Designer.cs
@@ -33,8 +33,8 @@ namespace DesktopApp
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.labelUsername = new System.Windows.Forms.Label();
             this.labelPassword = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxContrasenya = new System.Windows.Forms.TextBox();
             this.buttonIniciarSesion = new System.Windows.Forms.Button();
             this.linkLabelOlvidarContra = new System.Windows.Forms.LinkLabel();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -48,55 +48,50 @@ namespace DesktopApp
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(108, 117);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(81, 95);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(220, 198);
+            this.pictureBox1.Size = new System.Drawing.Size(165, 161);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // labelUsername
             // 
             this.labelUsername.AutoSize = true;
-            this.labelUsername.Location = new System.Drawing.Point(400, 132);
-            this.labelUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelUsername.Location = new System.Drawing.Point(300, 107);
             this.labelUsername.Name = "labelUsername";
-            this.labelUsername.Size = new System.Drawing.Size(73, 17);
+            this.labelUsername.Size = new System.Drawing.Size(55, 13);
             this.labelUsername.TabIndex = 1;
             this.labelUsername.Text = "Username";
             // 
             // labelPassword
             // 
             this.labelPassword.AutoSize = true;
-            this.labelPassword.Location = new System.Drawing.Point(400, 222);
-            this.labelPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPassword.Location = new System.Drawing.Point(300, 180);
             this.labelPassword.Name = "labelPassword";
-            this.labelPassword.Size = new System.Drawing.Size(69, 17);
+            this.labelPassword.Size = new System.Drawing.Size(53, 13);
             this.labelPassword.TabIndex = 2;
             this.labelPassword.Text = "Password";
             // 
-            // textBox1
+            // textBoxUsername
             // 
-            this.textBox1.Location = new System.Drawing.Point(404, 167);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(276, 22);
-            this.textBox1.TabIndex = 3;
+            this.textBoxUsername.Location = new System.Drawing.Point(303, 136);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(208, 20);
+            this.textBoxUsername.TabIndex = 3;
             // 
-            // textBox2
+            // textBoxContrasenya
             // 
-            this.textBox2.Location = new System.Drawing.Point(404, 258);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(276, 22);
-            this.textBox2.TabIndex = 4;
+            this.textBoxContrasenya.Location = new System.Drawing.Point(303, 210);
+            this.textBoxContrasenya.Name = "textBoxContrasenya";
+            this.textBoxContrasenya.Size = new System.Drawing.Size(208, 20);
+            this.textBoxContrasenya.TabIndex = 4;
+            this.textBoxContrasenya.UseSystemPasswordChar = true;
             // 
             // buttonIniciarSesion
             // 
-            this.buttonIniciarSesion.Location = new System.Drawing.Point(567, 334);
-            this.buttonIniciarSesion.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonIniciarSesion.Location = new System.Drawing.Point(425, 271);
             this.buttonIniciarSesion.Name = "buttonIniciarSesion";
-            this.buttonIniciarSesion.Size = new System.Drawing.Size(115, 28);
+            this.buttonIniciarSesion.Size = new System.Drawing.Size(86, 23);
             this.buttonIniciarSesion.TabIndex = 5;
             this.buttonIniciarSesion.Text = "Iniciar Sesion";
             this.buttonIniciarSesion.UseVisualStyleBackColor = true;
@@ -105,10 +100,9 @@ namespace DesktopApp
             // linkLabelOlvidarContra
             // 
             this.linkLabelOlvidarContra.AutoSize = true;
-            this.linkLabelOlvidarContra.Location = new System.Drawing.Point(400, 299);
-            this.linkLabelOlvidarContra.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabelOlvidarContra.Location = new System.Drawing.Point(300, 243);
             this.linkLabelOlvidarContra.Name = "linkLabelOlvidarContra";
-            this.linkLabelOlvidarContra.Size = new System.Drawing.Size(196, 17);
+            this.linkLabelOlvidarContra.Size = new System.Drawing.Size(148, 13);
             this.linkLabelOlvidarContra.TabIndex = 6;
             this.linkLabelOlvidarContra.TabStop = true;
             this.linkLabelOlvidarContra.Text = "¿Has olvidado la contraseña?";
@@ -118,16 +112,18 @@ namespace DesktopApp
             this.panel1.Controls.Add(this.pb_minimize);
             this.panel1.Controls.Add(this.pb_close);
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(814, 30);
+            this.panel1.Size = new System.Drawing.Size(610, 24);
             this.panel1.TabIndex = 7;
             // 
             // pb_minimize
             // 
             this.pb_minimize.Image = ((System.Drawing.Image)(resources.GetObject("pb_minimize.Image")));
-            this.pb_minimize.Location = new System.Drawing.Point(746, 2);
+            this.pb_minimize.Location = new System.Drawing.Point(560, 2);
+            this.pb_minimize.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pb_minimize.Name = "pb_minimize";
-            this.pb_minimize.Size = new System.Drawing.Size(25, 25);
+            this.pb_minimize.Size = new System.Drawing.Size(19, 20);
             this.pb_minimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pb_minimize.TabIndex = 9;
             this.pb_minimize.TabStop = false;
@@ -136,9 +132,10 @@ namespace DesktopApp
             // pb_close
             // 
             this.pb_close.Image = ((System.Drawing.Image)(resources.GetObject("pb_close.Image")));
-            this.pb_close.Location = new System.Drawing.Point(777, 3);
+            this.pb_close.Location = new System.Drawing.Point(583, 2);
+            this.pb_close.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pb_close.Name = "pb_close";
-            this.pb_close.Size = new System.Drawing.Size(25, 25);
+            this.pb_close.Size = new System.Drawing.Size(19, 20);
             this.pb_close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pb_close.TabIndex = 8;
             this.pb_close.TabStop = false;
@@ -146,19 +143,18 @@ namespace DesktopApp
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(814, 478);
+            this.ClientSize = new System.Drawing.Size(610, 388);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.linkLabelOlvidarContra);
             this.Controls.Add(this.buttonIniciarSesion);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxContrasenya);
+            this.Controls.Add(this.textBoxUsername);
             this.Controls.Add(this.labelPassword);
             this.Controls.Add(this.labelUsername);
             this.Controls.Add(this.pictureBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "FormLogin";
             this.Text = "FormLogin";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -175,8 +171,8 @@ namespace DesktopApp
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label labelUsername;
         private System.Windows.Forms.Label labelPassword;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxContrasenya;
         private System.Windows.Forms.Button buttonIniciarSesion;
         private System.Windows.Forms.LinkLabel linkLabelOlvidarContra;
         private System.Windows.Forms.Panel panel1;
--- Proyecto2Desktop/DesktopApp/DesktopApp/Models/UsuarisOrm.cs
+++ Proyecto2Desktop/DesktopApp/DesktopApp/Models/UsuarisOrm.cs
@@ -1,13 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DesktopApp.Models;
 
 namespace DesktopApp.Models
 {
     public static class UsuarisOrm
     {
+        
+        public static Boolean SelectLogin(string correu, string contrasenya)
+        {
+            Boolean acceptar;
 
-    }
+            usuaris user =  Orm.bd.usuaris
+                .Where (u => u.correo == correu )
+                .First();
+
+            Boolean passcorrecto = BCrypt.Net.BCrypt.EnhancedVerify(contrasenya, user.contrasenya, BCrypt.Net.HashType.SHA512);
+
+            if (passcorrecto)
+            {
+                System.Windows.Forms.MessageBox.Show("El usuario o la contrasenya són correctos!");
+                acceptar = true;
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("El usuario o la contrasenya són incorrectos!");
+                acceptar = false;
+            }
+
+            return acceptar;
+         }
+
+
+
+
+
+
+            /* public static Boolean SelectCorreuContra(String correu, String contrasenya)
+             {
+
+                 String missatge = "Error";
+                 String missatge1 = "Correcte";
+                 String contraencrypt;
+                 Boolean comprovar = false;
+                 SqlCommand sentencia = new SqlCommand();
+
+
+                 try
+                 {
+                     Orm.bd.Open();
+                     SqlDataReader dades;
+                     DataTable taula = new DataTable();
+
+                     sentencia.Connection = BDaluSQLConnection.conexio;
+                     sentencia.CommandText = "select correu,contrasenya FROM usuaris WHERE correu = @correu ";
+
+                     sentencia.Parameters.Clear();
+                     sentencia.Parameters.AddWithValue("@correu", correu);
+
+
+                     dades = sentencia.ExecuteReader();
+
+                     if (dades.Read())
+                     {
+
+
+                         contraencrypt = dades["contrasenya"].ToString();
+                         Boolean passcorrecto = BCrypt.Net.BCrypt.EnhancedVerify(contrasenya, contraencrypt, BCrypt.Net.HashType.SHA512);
+                         if (passcorrecto)
+                         {
+                             comprovar = true;
+                             System.Windows.Forms.MessageBox.Show(missatge1);
+
+                         }
+
+                     }
+
+
+
+
+
+                 }
+                 catch (SqlException ex)
+                 {
+                     System.Windows.Forms.MessageBox.Show(missatge);
+                     throw;
+                 }
+
+                 BDaluSQLConnection.conexio.Close();
+                 return comprovar;
+
+             }*/
+        }
 }
