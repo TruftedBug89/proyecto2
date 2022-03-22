355be2c29d11c5c6f0b7a6e7835f606a5c3cc5d8
marcph100 <98086516+marcph100@users.noreply.github.com>
2022-03-22 13:21:30 +0100

    APIUsersRols

---
+++ API/ApiProyect/ApiProyect/Controllers/rolsController.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiProyect.Models;
+
+namespace ApiProyect.Controllers
+{
+    public class rolsController : ApiController
+    {
+        private frase_aluEntities db = new frase_aluEntities();
+
+        // GET: api/rols
+        public IQueryable<rols> Getrols()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+            return db.rols;
+        }
+
+        // GET: api/rols/5
+        [ResponseType(typeof(rols))]
+        public async Task<IHttpActionResult> Getrols(int id)
+        {
+            IHttpActionResult result;
+            db.Configuration.LazyLoadingEnabled = false;
+           
+            rols _rols = await db.rols
+                                .Include("nom")
+                                .Where(c => c.id == id)
+                                .FirstOrDefaultAsync();
+            if (_rols == null)
+            {
+                result = NotFound();
+            }
+            else
+            {
+                result = Ok(_rols);
+            }
+            return result;
+        }
+
+
+        [HttpGet]
+        [Route("api/rols/nom/{nom}")]
+        public async Task<IHttpActionResult> Findnom(String nom)
+        {
+            IHttpActionResult result;
+            db.Configuration.LazyLoadingEnabled = false;
+
+            List<rols> _rols = db.rols
+                              .Where(c => c.nom.Contains(nom))
+                              .ToList();
+
+            return Ok(_rols);
+
+
+        }
+
+        // PUT: api/rols/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putrols(int id, rols _rols)
+        {
+            IHttpActionResult result;
+            String missatge = "";
+            if (!ModelState.IsValid)
+            {
+                result = BadRequest(ModelState);
+            }
+            else
+            {
+                if (id != _rols.id)
+                {
+                    result = BadRequest();
+                }
+                else
+                {
+                    db.Entry(_rols).State = EntityState.Modified;
+
+
+                    try
+                    {
+                        await db.SaveChangesAsync();
+                        result = StatusCode(HttpStatusCode.NoContent);
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!rolsExists(id))
+                        {
+                            result = NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    catch (DbUpdateException ex)
+                    {
+                        SqlException sqlException = (SqlException)ex.InnerException.InnerException;
+                        missatge = Clases.Error.MissatgeError(sqlException);
+                        result = BadRequest(missatge);
+
+                        throw;
+                    }
+                }
+
+            }
+
+
+            return result;
+        }
+
+        // POST: api/rols
+        [ResponseType(typeof(rols))]
+        public async Task<IHttpActionResult> Postrols(rols _rols)
+        {
+            IHttpActionResult result;
+
+            if (!ModelState.IsValid)
+            {
+                result = BadRequest(ModelState);
+            }
+            else
+            {
+
+                db.rols.Add(_rols);
+                String missatge = "";
+
+                try
+                {
+                    await db.SaveChangesAsync();
+                    result = CreatedAtRoute("DefaultApi", new { id = _rols.id }, _rols);
+                }
+                catch (DbUpdateException ex)
+                {
+                    SqlException sqlException = (SqlException)ex.InnerException.InnerException;
+                    missatge = Clases.Error.MissatgeError(sqlException);
+                    result = BadRequest(missatge);
+
+                    throw;
+                }
+            }
+            return result;
+        }
+
+        // DELETE: api/rols/5
+        [ResponseType(typeof(rols))]
+        public async Task<IHttpActionResult> Deleterols(int id)
+        {
+            IHttpActionResult result;
+            rols _rols = await db.rols.FindAsync(id);
+            if (_rols == null)
+            {
+                result = NotFound();
+            }
+            else
+            {
+                String missatge = "";
+                try
+                {
+                    db.rols.Remove(_rols);
+                    await db.SaveChangesAsync();
+                    result = Ok(_rols);
+                }
+                catch (DbUpdateException ex)
+                {
+                    SqlException sqlException = (SqlException)ex.InnerException.InnerException;
+                    missatge = Clases.Error.MissatgeError(sqlException);
+                    result = BadRequest(missatge);
+
+                    throw;
+                }
+
+            }
+
+
+
+            return result;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool rolsExists(int id)
+        {
+            return db.rols.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ API/ApiProyect/ApiProyect/Controllers/usuarisController.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiProyect.Models;
+
+namespace ApiProyect.Controllers
+{
+    public class usuarisController : ApiController
+    {
+        private frase_aluEntities db = new frase_aluEntities();
+
+        // GET: api/usuaris
+        public IQueryable<usuaris> Getusuaris()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+            return db.usuaris;
+        }
+
+        // GET: api/usuaris/5
+        [ResponseType(typeof(usuaris))]
+        public async Task<IHttpActionResult> Getusuaris(int id)
+        {
+            IHttpActionResult result;
+            db.Configuration.LazyLoadingEnabled = false;
+            //usuaris usuaris = await db.usuaris.FindAsync(id);
+            usuaris _usuari = await db.usuaris
+                                .Include("nom")
+                                .Where(c => c.id == id)
+                                .FirstOrDefaultAsync();
+            if (_usuari == null)
+            {
+                result = NotFound();
+            }
+            else
+            {
+                result = Ok(_usuari);
+            }
+            return result;
+
+        }
+
+        [HttpGet]
+        [Route("api/usuaris/nom/{nom}")]
+        public async Task<IHttpActionResult> findnom(String nom)
+        {
+            IHttpActionResult result;
+            db.Configuration.LazyLoadingEnabled = false;
+
+            List<usuaris> _usuaris = db.usuaris
+                                        .Where(c => c.nom.Contains(nom))
+                                        .ToList();
+
+            return Ok(_usuaris);
+
+
+        }
+        // PUT: api/usuaris/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putusuaris(int id, usuaris _usuaris)
+        {
+            IHttpActionResult result;
+            String missatge = "";
+            if (!ModelState.IsValid)
+            {
+                result = BadRequest(ModelState);
+            }
+            else
+            {
+                if (id != _usuaris.id)
+                {
+                    result = BadRequest();
+                }
+                else
+                {
+                    db.Entry(_usuaris).State = EntityState.Modified;
+
+
+                    try
+                    {
+                        await db.SaveChangesAsync();
+                        result = StatusCode(HttpStatusCode.NoContent);
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!usuarisExists(id))
+                        {
+                            result = NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    catch (DbUpdateException ex)
+                    {
+                        SqlException sqlException = (SqlException)ex.InnerException.InnerException;
+                        missatge = Clases.Error.MissatgeError(sqlException);
+                        result = BadRequest(missatge);
+
+                        throw;
+                    }
+                }
+
+            }
+
+
+            return result;
+        }
+
+        // POST: api/usuaris
+        [ResponseType(typeof(usuaris))]
+        public async Task<IHttpActionResult> Postusuaris(usuaris _usuaris)
+        {
+            IHttpActionResult result;
+
+            if (!ModelState.IsValid)
+            {
+                result = BadRequest(ModelState);
+            }
+            else
+            {
+
+                db.usuaris.Add(_usuaris);
+                String missatge = "";
+
+                try
+                {
+                    await db.SaveChangesAsync();
+                    result = CreatedAtRoute("DefaultApi", new { id = _usuaris.id }, _usuaris);
+                }
+                catch (DbUpdateException ex)
+                {
+                    SqlException sqlException = (SqlException)ex.InnerException.InnerException;
+                    missatge = Clases.Error.MissatgeError(sqlException);
+                    result = BadRequest(missatge);
+
+                    throw;
+                }
+
+
+            }
+
+
+
+            return result;
+        }
+
+        // DELETE: api/usuaris/5
+        [ResponseType(typeof(usuaris))]
+        public async Task<IHttpActionResult> Deleteusuaris(int id)
+        {
+            IHttpActionResult result;
+            usuaris _usuaris = await db.usuaris.FindAsync(id);
+            if (_usuaris == null)
+            {
+                result = NotFound();
+            }
+            else
+            {
+                String missatge = "";
+                try
+                {
+                    db.usuaris.Remove(_usuaris);
+                    await db.SaveChangesAsync();
+                    result = Ok(_usuaris);
+                }
+                catch (DbUpdateException ex)
+                {
+                    SqlException sqlException = (SqlException)ex.InnerException.InnerException;
+                    missatge = Clases.Error.MissatgeError(sqlException);
+                    result = BadRequest(missatge);
+
+                    throw;
+                }
+
+            }
+
+
+
+            return result;
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool usuarisExists(int id)
+        {
+            return db.usuaris.Count(e => e.id == id) > 0;
+        }
+    }
+
+}
