f6811df232d13a90075d50f80889324286462161
david345672 <davidmur12@gmail.com>
2022-04-26 18:48:26 +0200

    update notificacions

---
+++ API/ApiProyect/ApiProyect/Controllers/notificacionsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiProyect.Models;
+
+namespace ApiProyect.Controllers
+{
+    public class notificacionsController : ApiController
+    {
+        private frase_aluEntities db = new frase_aluEntities();
+
+        // GET: api/notificacions
+        public IQueryable<notificacions> Getnotificacions()
+        {
+            return db.notificacions
+                .Include("usuaris");
+        }
+
+        // GET: api/notificacions/5
+        [ResponseType(typeof(notificacions))]
+        public async Task<IHttpActionResult> Getnotificacions(int id)
+        {
+            notificacions notificacions = await db.notificacions.FindAsync(id);
+            if (notificacions == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(notificacions);
+        }
+
+        // PUT: api/notificacions/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putnotificacions(int id, notificacions notificacions)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != notificacions.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(notificacions).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!notificacionsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/notificacions
+        [ResponseType(typeof(notificacions))]
+        public async Task<IHttpActionResult> Postnotificacions(notificacions notificacions)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.notificacions.Add(notificacions);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = notificacions.id }, notificacions);
+        }
+
+        // DELETE: api/notificacions/5
+        [ResponseType(typeof(notificacions))]
+        public async Task<IHttpActionResult> Deletenotificacions(int id)
+        {
+            notificacions notificacions = await db.notificacions.FindAsync(id);
+            if (notificacions == null)
+            {
+                return NotFound();
+            }
+
+            db.notificacions.Remove(notificacions);
+            await db.SaveChangesAsync();
+
+            return Ok(notificacions);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool notificacionsExists(int id)
+        {
+            return db.notificacions.Count(e => e.id == id) > 0;
+        }
+    }
+}
